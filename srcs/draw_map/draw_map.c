9557131bc9cb85ca092f6ab01b0ee06be5c99a93
epainter <gavgavov@gmail.com>
2019-12-15 22:08:27 +0300

    some refactoring

--- srcs/draw_map/draw_map.c
+++ srcs/draw_map/draw_map.c
@@ -6,7 +6,7 @@
 /*   By: epainter <epainter@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/11 23:16:06 by epainter          #+#    #+#             */
-/*   Updated: 2019/12/15 18:40:21 by epainter         ###   ########.fr       */
+/*   Updated: 2019/12/15 22:01:30 by epainter         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,39 +14,33 @@
 
 static	void		draw_x_lines(t_map map, t_trasform_params p, int i)
 {
-	uint32_t	tmp_color[2];
 	int			j;
+	t_pixel		tmp_pixel[2];
 
 	j = -1;
 	while (map.map[i][++j].is_exist_next == 1)
 	{
-		tmp_color[0] = map.map[i][j].color;
-		tmp_color[1] = map.map[i][j + 1].color;
-		map.map[i][j].color = color_set(map.map[i][j], map);
-		map.map[i][j + 1].color = color_set(map.map[i][j + 1], map);
-		line(all_trasforms(map.map[i][j], p),\
-				all_trasforms(map.map[i][j + 1], p), map.img_data);
-		map.map[i][j].color = tmp_color[0];
-		map.map[i][j + 1].color = tmp_color[1];
+		tmp_pixel[0] = all_trasforms(map.map[i][j], p);
+		tmp_pixel[1] = all_trasforms(map.map[i][j + 1], p);
+		tmp_pixel[0].color = color_set(map.map[i][j], map);
+		tmp_pixel[1].color = color_set(map.map[i][j + 1], map);
+		line(tmp_pixel[0], tmp_pixel[1], map.img_data);
 	}
 }
 
 static	void		draw_y_lines(t_map map, t_trasform_params p, int i)
 {
 	int			j;
-	uint32_t	tmp_color[2];
+	t_pixel		tmp_pixel[2];
 
 	j = -1;
 	while (j++ > INT32_MIN)
 	{
-		tmp_color[0] = map.map[i][j].color;
-		tmp_color[1] = map.map[i + 1][j].color;
-		map.map[i][j].color = color_set(map.map[i][j], map);
-		map.map[i + 1][j].color = color_set(map.map[i + 1][j], map);
-		line(all_trasforms(map.map[i][j], p),\
-			all_trasforms(map.map[i + 1][j], p), map.img_data);
-		map.map[i][j].color = tmp_color[0];
-		map.map[i + 1][j].color = tmp_color[1];
+		tmp_pixel[0] = all_trasforms(map.map[i][j], p);
+		tmp_pixel[1] = all_trasforms(map.map[i + 1][j], p);
+		tmp_pixel[0].color = color_set(map.map[i][j], map);
+		tmp_pixel[1].color = color_set(map.map[i + 1][j], map);
+		line(tmp_pixel[0], tmp_pixel[1], map.img_data);
 		if (!(map.map[i][j].is_exist_next\
 			&& map.map[i + 1][j].is_exist_next))
 			break ;
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: epainter <epainter@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/01 13:08:43 by epainter          #+#    #+#             */
-/*   Updated: 2019/12/15 15:13:24 by epainter         ###   ########.fr       */
+/*   Updated: 2019/12/15 21:26:24 by epainter         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	print_menu(t_map p)
 	mlx_string_put(p.mlx, p.window, 1500, 20, 0xffffff, "MOVE: ARROW");
 	mlx_string_put(p.mlx, p.window, 1500, 40, 0xffffff, "ZOOM: -/+");
 	mlx_string_put(p.mlx, p.window, 1500,\
-	60, 0xffFFFFFF, "ROTATE: X: 4/6 Y: 2/8 Z: 1/9");
+	60, 0xffffff, "ROTATE: X: 4/6 Y: 2/8 Z: 1/9");
 	mlx_string_put(p.mlx, p.window, 1500, 80, 0xffffff, "EXIT: ESC");
 	mlx_string_put(p.mlx, p.window, 1500, 100, 0xffffff, "COLOR SET: Z-C");
 	mlx_string_put(p.mlx, p.window, 1500, 120, 0xffffff, "ISOMETRIC: I");
